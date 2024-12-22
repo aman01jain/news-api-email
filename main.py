30596bbdbee5de446397eee64be6a40738634ec1
Aman <aman01jain@gmail.com>
2024-12-22 10:09:28 -0700

    changed body structure

--- main.py
+++ main.py
@@ -12,13 +12,12 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article title and description
-body = " "
-for article in content["articles"][0:10]:
-    if article["title"] is not None:
-        body = "Subject: Today's news" \
-               + "\n" +body +article["title"] + "\n" \
-               + article["description"] +"\n" +\
-               article["url"] + 2*"\n"
+body = "Subject: Today's news" + "\n"
+for article in content["articles"][:20]:
+    if (article["description"] is not None) and (article["title"] is not None):
+        body = body+ article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"] + 2*"\n"
 
     body = body.encode("utf-8")
     email_send(message=body)
